236fc19255f28d0ababa08eb172b352d74420511
yang <752657975@qq.com>
2024-06-27 07:27:29 +0800

    注册中心

--- web/register/etcd.go
+++ web/register/etcd.go
@@ -8,69 +8,89 @@ import (
 	"time"
 )
 
+// CreateEtcdCli 创建并返回一个etcd客户端
 func CreateEtcdCli(option Option) (*clientv3.Client, error) {
+	// 使用传入的选项创建一个etcd客户端
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   option.Endpoints,   //节点
-		DialTimeout: option.DialTimeout, //超过5秒钟连不上超时
+		Endpoints:   option.Endpoints,   // etcd节点列表
+		DialTimeout: option.DialTimeout, // 连接超时时间
 	})
-	return cli, err
+	return cli, err // 返回客户端和错误（如果有）
 }
 
+// RegEtcdService 在etcd中注册服务
 func RegEtcdService(cli *clientv3.Client, serviceName string, host string, port int) error {
+	// 创建上下文，设置超时时间为1秒
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel() // 确保函数返回前取消上下文
+	// 在etcd中注册服务，键为服务名称，值为服务地址和端口
 	_, err := cli.Put(ctx, serviceName, fmt.Sprintf("%s:%d", host, port))
-	return err
+	return err // 返回注册服务时的错误（如果有）
 }
 
+// GetEtcdValue 从etcd中获取服务的值
 func GetEtcdValue(cli *clientv3.Client, serviceName string) (string, error) {
+	// 创建上下文，设置超时时间为1秒
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel() // 确保函数返回前取消上下文
+	// 从etcd中获取服务的值
 	v, err := cli.Get(ctx, serviceName)
 	if err != nil {
-		return "", err
+		return "", err // 如果获取值失败，返回错误
 	}
+	// 获取键值对列表
 	kvs := v.Kvs
 	if len(kvs) == 0 {
-		return "", errors.New("no value")
+		return "", errors.New("no value") // 如果没有值，返回错误
 	}
-	return string(kvs[0].Value), err
+	return string(kvs[0].Value), err // 返回第一个键值对的值和错误（如果有）
 }
 
+// MsEtcdRegister 代表一个etcd注册器
 type MsEtcdRegister struct {
-	cli *clientv3.Client
+	cli *clientv3.Client // etcd客户端
 }
 
+// CreateCli 创建etcd客户端
 func (r *MsEtcdRegister) CreateCli(option Option) error {
+	// 创建etcd客户端
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   option.Endpoints,   //节点
-		DialTimeout: option.DialTimeout, //超过5秒钟连不上超时
+		Endpoints:   option.Endpoints,   // etcd节点列表
+		DialTimeout: option.DialTimeout, // 连接超时时间
 	})
-	r.cli = cli
-	return err
+	r.cli = cli // 将创建的客户端赋值给结构体的cli字段
+	return err  // 返回可能的错误
 }
 
+// RegisterService 在etcd中注册服务
 func (r *MsEtcdRegister) RegisterService(serviceName string, host string, port int) error {
+	// 创建一个上下文，设置超时时间为1秒
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel() // 确保函数返回前取消上下文
+	// 在etcd中注册服务，键为服务名称，值为服务地址和端口
 	_, err := r.cli.Put(ctx, serviceName, fmt.Sprintf("%s:%d", host, port))
-	return err
+	return err // 返回注册服务时的错误（如果有）
 }
 
+// GetValue 从etcd中获取服务的值
 func (r *MsEtcdRegister) GetValue(serviceName string) (string, error) {
+	// 创建一个上下文，设置超时时间为1秒
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel() // 确保函数返回前取消上下文
+	// 从etcd中获取服务的值
 	v, err := r.cli.Get(ctx, serviceName)
 	if err != nil {
-		return "", err
+		return "", err // 如果获取值失败，返回错误
 	}
+	// 获取键值对列表
 	kvs := v.Kvs
 	if len(kvs) == 0 {
-		return "", errors.New("no value")
+		return "", errors.New("no value") // 如果没有值，返回错误
 	}
-	return string(kvs[0].Value), err
+	return string(kvs[0].Value), err // 返回第一个键值对的值和错误（如果有）
 }
 
+// Close 关闭etcd客户端
 func (r *MsEtcdRegister) Close() error {
-	return r.cli.Close()
+	return r.cli.Close() // 关闭etcd客户端
 }
--- web/register/nacos.go
+++ web/register/nacos.go
@@ -9,22 +9,23 @@ import (
 )
 
 func CreateNacosClient() (naming_client.INamingClient, error) {
-	// 创建clientConfig的另一种方式
+	// 创建 clientConfig 的另一种方式
 	clientConfig := *constant.NewClientConfig(
-		constant.WithNamespaceId(""), //当namespace是public时，此处填空字符串。
-		constant.WithTimeoutMs(5000),
-		constant.WithNotLoadCacheAtStart(true),
-		constant.WithLogDir("/tmp/nacos/log"),
-		constant.WithCacheDir("/tmp/nacos/cache"),
-		constant.WithLogLevel("debug"),
+		constant.WithNamespaceId(""),              // 当 namespace 是 public 时，此处填空字符串。
+		constant.WithTimeoutMs(5000),              // 设置超时时间为 5000 毫秒
+		constant.WithNotLoadCacheAtStart(true),    // 不在启动时加载缓存
+		constant.WithLogDir("/tmp/nacos/log"),     // 设置日志目录
+		constant.WithCacheDir("/tmp/nacos/cache"), // 设置缓存目录
+		constant.WithLogLevel("debug"),            // 设置日志级别为 debug
 	)
-	// 创建serverConfig的另一种方式
+
+	// 创建 serverConfig 的另一种方式
 	serverConfigs := []constant.ServerConfig{
 		*constant.NewServerConfig(
-			"127.0.0.1",
-			8848,
-			constant.WithScheme("http"),
-			constant.WithContextPath("/nacos"),
+			"127.0.0.1",                        // 服务器 IP 地址
+			8848,                               // 端口号
+			constant.WithScheme("http"),        // 使用 HTTP 协议
+			constant.WithContextPath("/nacos"), // 设置上下文路径
 		),
 	}
 
@@ -32,109 +33,115 @@ func CreateNacosClient() (naming_client.INamingClient, error) {
 	// 创建服务发现客户端的另一种方式 (推荐)
 	namingClient, err := clients.NewNamingClient(
 		vo.NacosClientParam{
-			ClientConfig:  &clientConfig,
-			ServerConfigs: serverConfigs,
+			ClientConfig:  &clientConfig, // 客户端配置
+			ServerConfigs: serverConfigs, // 服务器配置
 		},
 	)
 	if err != nil {
-		return nil, err
+		return nil, err // 如果创建客户端失败，返回错误
 	}
-	return namingClient, nil
+	return namingClient, nil // 返回创建的命名客户端
 }
 
 func RegService(namingClient naming_client.INamingClient, serviceName string, host string, port int) error {
+	// 注册服务实例
 	_, err := namingClient.RegisterInstance(vo.RegisterInstanceParam{
-		Ip:          host,
-		Port:        uint64(port),
-		ServiceName: serviceName,
-		Weight:      10,
-		Enable:      true,
-		Healthy:     true,
-		Ephemeral:   true,
-		Metadata:    map[string]string{"idc": "shanghai"},
-		//ClusterName: "cluster-a", // 默认值DEFAULT
-		//GroupName:   "group-a",   // 默认值DEFAULT_GROUP
+		Ip:          host,                                 // 实例的 IP 地址
+		Port:        uint64(port),                         // 实例的端口号
+		ServiceName: serviceName,                          // 服务名称
+		Weight:      10,                                   // 实例的权重
+		Enable:      true,                                 // 实例是否启用
+		Healthy:     true,                                 // 实例是否健康
+		Ephemeral:   true,                                 // 实例是否为临时实例
+		Metadata:    map[string]string{"idc": "shanghai"}, // 实例的元数据
+		//ClusterName: "cluster-a",            // 集群名称，默认值为 DEFAULT
+		//GroupName:   "group-a",              // 组名称，默认值为 DEFAULT_GROUP
 	})
 
-	return err
+	return err // 返回注册结果中的错误信息
 }
 
 func GetInstance(namingClient naming_client.INamingClient, serviceName string) (string, uint64, error) {
-	// SelectOneHealthyInstance将会按加权随机轮询的负载均衡策略返回一个健康的实例
-	// 实例必须满足的条件：health=true,enable=true and weight>0
+	// SelectOneHealthyInstance 将会按加权随机轮询的负载均衡策略返回一个健康的实例
+	// 实例必须满足的条件：health=true, enable=true 和 weight>0
 	instance, err := namingClient.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
-		ServiceName: serviceName,
-		//GroupName:   "group-a",             // 默认值DEFAULT_GROUP
-		//Clusters:    []string{"cluster-a"}, // 默认值DEFAULT
+		ServiceName: serviceName, // 服务名称
+		//GroupName:   "group-a",             // 组名称，默认值为 DEFAULT_GROUP
+		//Clusters:    []string{"cluster-a"}, // 集群名称，默认值为 DEFAULT
 	})
 	if err != nil {
-		return "", uint64(0), err
+		return "", uint64(0), err // 如果获取实例失败，返回错误
 	}
-	return instance.Ip, instance.Port, nil
+	return instance.Ip, instance.Port, nil // 返回实例的 IP 和端口号
 }
 
-//CreateCli(option Option)  error
-//	RegisterService(serviceName string, host string, port int) error
-//	GetValue(serviceName string) (string, error)
-//	Close() error
+// CreateCli(option Option)  error
+// RegisterService(serviceName string, host string, port int) error
+// GetValue(serviceName string) (string, error)
+// Close() error
 
 type MsNacosRegister struct {
-	cli naming_client.INamingClient
+	cli naming_client.INamingClient // Nacos 客户端
 }
 
 func (r *MsNacosRegister) CreateCli(option Option) error {
-	// 创建clientConfig的另一种方式
-	//clientConfig := *constant.NewClientConfig(
-	//	constant.WithNamespaceId(""), //当namespace是public时，此处填空字符串。
-	//	constant.WithTimeoutMs(5000),
-	//	constant.WithNotLoadCacheAtStart(true),
-	//	constant.WithLogDir("/tmp/nacos/log"),
-	//	constant.WithCacheDir("/tmp/nacos/cache"),
-	//	constant.WithLogLevel("debug"),
-	//)
+	// 创建 clientConfig 的另一种方式
+	// clientConfig := *constant.NewClientConfig(
+	//    constant.WithNamespaceId(""), // 当 namespace 是 public 时，此处填空字符串。
+	//    constant.WithTimeoutMs(5000),
+	//    constant.WithNotLoadCacheAtStart(true),
+	//    constant.WithLogDir("/tmp/nacos/log"),
+	//    constant.WithCacheDir("/tmp/nacos/cache"),
+	//    constant.WithLogLevel("debug"),
+	// )
 
 	// 创建服务发现客户端
-	// 创建服务发现客户端的另一种方式 (推荐)
+	// 创建服务发现客户端的另一种方式（推荐）
 	namingClient, err := clients.NewNamingClient(
 		vo.NacosClientParam{
-			ClientConfig:  option.NacosClientConfig,
-			ServerConfigs: option.NacosServerConfig,
+			ClientConfig:  option.NacosClientConfig, // Nacos 客户端配置
+			ServerConfigs: option.NacosServerConfig, // Nacos 服务器配置
 		},
 	)
 	if err != nil {
-		return err
+		return err // 返回错误
 	}
-	r.cli = namingClient
-	return nil
+	r.cli = namingClient // 赋值客户端
+	return nil           // 返回 nil 表示成功
 }
 
 func (r *MsNacosRegister) RegisterService(serviceName string, host string, port int) error {
+	// 注册服务实例
 	_, err := r.cli.RegisterInstance(vo.RegisterInstanceParam{
-		Ip:          host,
-		Port:        uint64(port),
-		ServiceName: serviceName,
-		Weight:      10,
-		Enable:      true,
-		Healthy:     true,
-		Ephemeral:   true,
-		Metadata:    map[string]string{"idc": "shanghai"},
-		//ClusterName: "cluster-a", // 默认值DEFAULT
-		//GroupName:   "group-a",   // 默认值DEFAULT_GROUP
+		Ip:          host,                                 // 实例的 IP 地址
+		Port:        uint64(port),                         // 实例的端口号
+		ServiceName: serviceName,                          // 服务名称
+		Weight:      10,                                   // 实例的权重
+		Enable:      true,                                 // 实例是否启用
+		Healthy:     true,                                 // 实例是否健康
+		Ephemeral:   true,                                 // 实例是否为临时实例
+		Metadata:    map[string]string{"idc": "shanghai"}, // 实例的元数据
+		// ClusterName: "cluster-a",            // 集群名称，默认值为 DEFAULT
+		// GroupName:   "group-a",              // 组名称，默认值为 DEFAULT_GROUP
 	})
-	return err
+	return err // 返回注册结果中的错误信息
 }
+
 func (r *MsNacosRegister) GetValue(serviceName string) (string, error) {
+	// 选择一个健康的实例
 	instance, err := r.cli.SelectOneHealthyInstance(vo.SelectOneHealthInstanceParam{
-		ServiceName: serviceName,
-		//GroupName:   "group-a",             // 默认值DEFAULT_GROUP
-		//Clusters:    []string{"cluster-a"}, // 默认值DEFAULT
+		ServiceName: serviceName, // 服务名称
+		// GroupName:   "group-a",             // 组名称，默认值为 DEFAULT_GROUP
+		// Clusters:    []string{"cluster-a"}, // 集群名称，默认值为 DEFAULT
 	})
 	if err != nil {
-		return "", err
+		return "", err // 如果获取实例失败，返回错误
 	}
+	// 返回实例的 IP 和端口号
 	return fmt.Sprintf("%s:%d", instance.Ip, instance.Port), nil
 }
 
 func (r *MsNacosRegister) Close() error {
+	// 关闭客户端
 	return nil
 }
